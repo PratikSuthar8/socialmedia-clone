3637794d308d23265269bde85b124b71aea35a76
Janick <136822147+PratikSuthar8@users.noreply.github.com>
2023-06-17 17:43:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const settingsmenu = document.querySelector('.settings-menu')
+
+const darkBtn = document.getElementById("dark-btn");
+
+
+const settingsMenuToggle = () => {
+    settingsmenu.classList.toggle("settings-menu-height");
+
+}
+darkBtn.onclick = function () {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme")
+}
